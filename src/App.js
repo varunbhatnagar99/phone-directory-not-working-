cd495faeb902a1eb3670d22b1178641b8b2124db
Varun Bhatnagar <varun41633@gmail.com>
2019-12-08 20:08:51 +0530

    Added ‘PhoneDirectory’ as smart component, renamed ‘App…

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
-import './App.css';
+import './ShowSubscribers.css';
 
-class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      subscribersListToShow: []
-    }
-  }
-
-  render() {
+import './ShowSubscribers.css';
+  
+render() {
+  
   return (
     <div>
       <Header heading="Phone Directory"/>
@@ -24,7 +18,7 @@ class App extends Component {
           </div>
 
           {
-           this.state.subscribersListToShow.map(sub => {
+             this.props.subscribersList.map(sub => {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
@@ -39,4 +33,4 @@ class App extends Component {
   );
 }
 }
-export default App;
+export default ShowSubscribers;
